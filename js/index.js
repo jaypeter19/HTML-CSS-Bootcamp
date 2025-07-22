2ba5c6042f52c5a0800619a1cbf9fbc284cd41e8
jaypeter19 <joaopedrofava19@gmail.com>
2025-07-21 22:54:09 -0300

    index.js added

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger-button');
     const mobileNav = document.querySelector('.mobileNav')
 
-
     hamburger.addEventListener('click', () => {
         mobileNav.classList.toggle('openDrawer');
     })
